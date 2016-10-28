8f37952893e2735e3e7c0872a22d074f74decdc3
yuweifeng1017 <522291511@qq.com>
2016-10-28 14:53:23 +0800

    Chapter 6 Storage

---
+++ app/src/main/java/com/example/administrator/storagetest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.administrator.storagetest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textAccount = (TextView) findViewById(R.id.account);
+        final TextView textPassword = (TextView) findViewById(R.id.password);
+        Button buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = textAccount.getText().toString();
+                String password = textPassword.getText().toString();
+                save(account,password);
+            }
+        });
+    }
+
+    public void save(String account, String password) {
+        FileOutputStream outputStream = null;
+        BufferedWriter writer = null;
+        try {
+            outputStream = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            writer.write("account:" + account + " password:" + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
